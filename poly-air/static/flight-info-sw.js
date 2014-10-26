2499d67b14a70b2f744ea91c4c80baa89d26eaca
Mounir Lamouri <mounir@lamouri.fr>
2014-10-25 23:13:40 -0700

    add localforage.js

--- poly-air/static/flight-info-sw.js
+++ poly-air/static/flight-info-sw.js
@@ -1,5 +1,7 @@
 "use strict";
 
+importScripts("lib/localforage.js");
+
 var baseUrl = new URL("/", this.location.href) + "";
 
 this.addEventListener("install", function(evt) {
@@ -11,10 +13,33 @@ this.addEventListener("activate", function(evt) {
 });
 
 this.addEventListener("push", function(evt) {
-  console.log(evt.data);
-  new Notification("push!", {
+  var data = evt.data.split(':');
+  var title = 'No Title';
+  var message = 'No Message';
+
+  new Notification(title, {
     serviceWorker: true,
-    body: 'push message',
+    body: message,
     icon: 'icons/icon-96.png'
   });
+
+  /*
+  if (data[0] == 'gate') {
+    title = 'Gate changed!';
+    message = 'New gate is ' + data[1];
+
+//    localforage.getItem('track').then(function(flight) {
+//      flight.depart.gate = data[1];
+//      localforage.setItem('track').then(function() {
+//      });
+//    });
+
+    new Notification(title, {
+      serviceWorker: true,
+      body: message,
+      icon: 'icons/icon-96.png'
+    });
+  }
+*/
+
 });
