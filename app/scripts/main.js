275c1ea88ec910168b0083dc46c7e49f649ff6ce
Matt Gaunt <matt@gauntface.co.uk>
2015-02-18 19:21:40 +0000

    Removing hasPermission, fixing up unsubscribe and attributing yahoo weather API

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -88,7 +88,7 @@ function enabledPushMessages() {
     // If the permission isn't granted change state of switch
     window.PushDemo.ui.setPushChecked(result === 'granted');
 
-    // If the permission wsa denied, show a message explaining
+    // If the permission was denied, show a message explaining
     // it's permanent 
     if (result === 'denied') {
       window.PushDemo.ui.showError('Ooops Notifications are Blocked', 
@@ -111,10 +111,25 @@ function disablePushMessages() {
           return;
         }
         
-        pushSubscription.unsubscribe().then(function() {
+        pushSubscription.unsubscribe().then(function(successful) {
+          console.log('Unsubscribed from push: ', successful);
+          if (!successful) {
+            // The unsubscribe was unsuccessful, but we can
+            // remove the subscriptionId from our server
+            // and notifications will stop
+            // This just may be in a bad state when the user returns
+            console.error('We were unable to unregister from push');
+          }
+
+          // TODO: Remove the subscription ID on your server
+
           window.PushDemo.ui.showGCMPushOptions(false);
         }).catch(function(e) {
-          window.PushDemo.ui.showGCMPushOptions(false);
+          console.log('Unsubscribtion error: ', e);
+          window.PushDemo.ui.showGCMPushOptions(true);
+
+          // Set the state of the push switch
+          window.PushDemo.ui.setPushChecked(true);
         });
 
         
@@ -138,6 +153,8 @@ function onServiceWorkerRegistered() {
     return;
   }
 
+  console.log('Notification.permission = ', Notification.permission);
+
   // If the notification permission is denied, it's a permanent block
   if (Notification.permission === 'denied') {
     window.PushDemo.ui.showError('Ooops Notifications are Blocked', 
@@ -146,61 +163,54 @@ function onServiceWorkerRegistered() {
     return;
   }
 
-  // Check if push is supported and what the current state is
-  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-    // Check if this service worker supports push
-    if (!serviceWorkerRegistration.pushManager) {
-      window.PushDemo.ui.showError('Ooops Push Isn\'t Supported',
-        '<p>This could be a few things.</p>' +
-        '<ol>' +
-        '<li>Make sure you are using Chrome Canary / Chrome version 42+</li>' +
-        '<li>Make sure you have Experimental Web Platform features enabled ' + 
-        'in Chrome flags (chrome://flags/#enable-experimental-web-platform-features)</li>' +
-        '<li>Make sure you have "gcm_sender_id" and "gcm_user_visible_only" defined' +
-        ' in your manifest</li>' +
-        '</ol>' +
-        'If both of the above are true, then please message ' +
-        '<a href="https://twitter.com/gauntface">@gauntface</a> as the ' +
-        'demo is probably broken.');
-      return;
-    }
+  if (Notification.permission === 'granted') {
+    // Check if push is supported and what the current state is
+    navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+      // Check if this service worker supports push
+      if (!serviceWorkerRegistration.pushManager) {
+        window.PushDemo.ui.showError('Ooops Push Isn\'t Supported',
+          '<p>This could be a few things.</p>' +
+          '<ol>' +
+          '<li>Make sure you are using Chrome Canary / Chrome version 42+</li>' +
+          '<li>Make sure you have Experimental Web Platform features enabled ' + 
+          'in Chrome flags (chrome://flags/#enable-experimental-web-platform-features)</li>' +
+          '<li>Make sure you have "gcm_sender_id" and "gcm_user_visible_only" defined' +
+          ' in your manifest</li>' +
+          '</ol>' +
+          'If both of the above are true, then please message ' +
+          '<a href="https://twitter.com/gauntface">@gauntface</a> as the ' +
+          'demo is probably broken.');
+        return;
+      }
 
-    // Check if we have permission for push messages already
-    serviceWorkerRegistration.pushManager.hasPermission().then(
-      function(pushPermissionStatus) {
-        // Once we have a service worker, check the current state
+      // Let's update our subscription details.
+      // This is required for this demo, you 
+      // will most likely want to assume the subscriptionId
+      // and endpoint are saved on your server and you don't
+      // need to do anything
+      serviceWorkerRegistration.pushManager.getSubscription()
+        .then(function(subscription) {
+          console.log('Push Subscription = ', subscription);
+          window.PushDemo.ui.setPushSwitchDisabled(false);
+          if (!subscription) {
+            // NOOP
+            return;
+          }
 
-        window.PushDemo.ui.setPushSwitchDisabled(false);
+          // Set the initial state of the push switch
+          window.PushDemo.ui.setPushChecked(true);
 
-        if (pushPermissionStatus === 'granted') {
-          // Let's update our subscription details.
-          // This is required for this demo, you 
-          // will most likely want to assume the subscriptionId
-          // and endpoint are saved on your server and you don't
-          // need to do anything
-          serviceWorkerRegistration.pushManager.getSubscription()
-            .then(function(subscription) {
-              if (!subscription) {
-                // NOOP
-                return;
-              }
-
-              // Set the initial state of the push switch
-              window.PushDemo.ui.setPushChecked(true);
-
-              // Update the current state with the 
-              // subscriptionid and endpoint
-              onPushSubscription(subscription);
-            })
-            .catch(function(e) {
-              // NOOP
-            });
-        }
-
-        // TODO: Should we update the subscriptionID just in case?
-        // SW's pushsubscriptionlost should handle any off scenario
-      });
-  });
+          // Update the current state with the
+          // subscriptionid and endpoint
+          onPushSubscription(subscription);
+        })
+        .catch(function(e) {
+          // NOOP
+        });
+    });
+  } else {
+    window.PushDemo.ui.setPushSwitchDisabled(false);
+  }
 }
 
 window.addEventListener('UIReady', function() {
