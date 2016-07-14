8094fb898ddd8bf0975f3073466bdfed8614eaff
Matt Gaunt <matt@gauntface.co.uk>
2016-07-14 11:44:48 -0700

    Switching to subarry

--- src/scripts/encryption/helpers.js
+++ src/scripts/encryption/helpers.js
@@ -5,7 +5,7 @@ function uint8ArrayToBase64Url(uint8Array, start, end) {
   end = end || uint8Array.byteLength;
 
   const base64 = window.btoa(
-    String.fromCharCode.apply(null, uint8Array.slice(start, end)));
+    String.fromCharCode.apply(null, uint8Array.subarray(start, end)));
   return base64
     .replace(/\=/g, '') // eslint-disable-line no-useless-escape
     .replace(/\+/g, '-')
