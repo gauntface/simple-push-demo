0bbd26ee674b3dcaf7119c91f25bbfba3684e403
Travis CI <gauntface@google.com>
2016-07-14 19:05:32 +0000

    Deploy to GitHub Pages

--- scripts/encryption/helpers.js
+++ scripts/encryption/helpers.js
@@ -6,7 +6,7 @@ function uint8ArrayToBase64Url(uint8Array, start, end) {
   start = start || 0;
   end = end || uint8Array.byteLength;
 
-  var base64 = window.btoa(String.fromCharCode.apply(null, uint8Array.slice(start, end)));
+  var base64 = window.btoa(String.fromCharCode.apply(null, uint8Array.subarray(start, end)));
   return base64.replace(/\=/g, '') // eslint-disable-line no-useless-escape
   .replace(/\+/g, '-').replace(/\//g, '_');
 }
