c7947606bd3f273b121e46de20687d62e42381c1
Matt Gaunt <matt@gauntface.co.uk>
2016-08-03 12:09:38 -0700

    Adding more appropriate badge image

--- src/service-worker.js
+++ src/service-worker.js
@@ -12,7 +12,7 @@ self.addEventListener('push', function(event) {
   const notificationOptions = {
     body: 'Thanks for sending this push msg.',
     icon: './images/icon-192x192.png',
-    badge: './images/icon-192x192.png',
+    badge: './images/icon-72x72.png',
     tag: 'simple-push-demo-notification',
     data: {
       url: 'https://developers.google.com/web/fundamentals/getting-started/push-notifications/'
