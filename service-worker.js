b5a0f8e1c7fa30a51f1a0459e2b5655ea5cc9eb1
Travis CI <gauntface@google.com>
2016-09-05 03:36:47 +0000

    Deploy to GitHub Pages

--- service-worker.js
+++ service-worker.js
@@ -11,8 +11,8 @@ self.addEventListener('push', function (event) {
   var notificationTitle = 'Hello';
   var notificationOptions = {
     body: 'Thanks for sending this push msg.',
-    icon: './images/icon-192x192.png',
-    badge: './images/icon-72x72.png',
+    icon: './images/logo-192x192.png',
+    badge: './images/logo-72x72.png',
     tag: 'simple-push-demo-notification',
     data: {
       url: 'https://developers.google.com/web/fundamentals/getting-started/push-notifications/'
