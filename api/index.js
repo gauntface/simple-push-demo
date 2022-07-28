169e7f1788370413ec88eab436b54afa0d05215c
Matt Gaunt <matt@gaunt.dev>
2022-07-28 10:42:11 -0700

    Update API to use express

--- api/index.js
+++ api/index.js
@@ -1,58 +1,63 @@
-import Fastify from 'fastify';
+import express from 'express';
 import https from 'https';
 
-// eslint-disable-next-line new-cap
-const fastify = Fastify({
-  // Set this to true for detailed logging:
-  logger: false,
+export const app = express();
+
+// Parse body as json when content-type: application/json
+app.use(express.json());
+
+// Set-up for CORs
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', process.env['ACCESS_CONTROL']);
+  res.setHeader('Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
+  next();
 });
 
-fastify.post('/api/v3/sendpush', async function(request, reply) {
-  reply.header('Access-Control-Allow-Origin', process.env['ACCESS_CONTROL']);
+app.post('/api/v3/sendpush', async function(request, res) {
+  try {
+    const requestData = request.body;
+    const httpsOptions = {
+      headers: requestData.headers,
+      method: 'POST',
+    };
+    const urlParts = new URL(requestData.endpoint);
+    httpsOptions.hostname = urlParts.hostname;
+    httpsOptions.port = urlParts.port;
+    httpsOptions.path = urlParts.pathname;
 
-  const requestData = JSON.parse(request.body);
+    const pushRequest = https.request(httpsOptions, function(pushResponse) {
+      let responseText = '';
 
-  const httpsOptions = {
-    headers: requestData.headers,
-    method: 'POST',
-  };
-  const urlParts = new URL(requestData.endpoint);
-  httpsOptions.hostname = urlParts.hostname;
-  httpsOptions.port = urlParts.port;
-  httpsOptions.path = urlParts.pathname;
+      pushResponse.on('data', function(chunk) {
+        responseText += chunk;
+      });
 
-  const pushRequest = https.request(httpsOptions, function(pushResponse) {
-    let responseText = '';
-
-    pushResponse.on('data', function(chunk) {
-      responseText += chunk;
+      pushResponse.on('end', function() {
+        res.status(pushResponse.statusCode);
+        res.send(responseText);
+        if (pushResponse.statusCode &&
+          (pushResponse.statusCode < 200 || pushResponse.statusCode > 299)) {
+          console.log(`Error: ${responseText}`);
+        }
+      });
     });
 
-    pushResponse.on('end', function() {
-      reply.code(pushResponse.statusCode);
-      reply.send(responseText);
-      if (pushResponse.statusCode &&
-        (pushResponse.statusCode < 200 || pushResponse.statusCode > 299)) {
-        console.log(`Error: ${responseText}`);
-      }
+    pushRequest.on('error', function(e) {
+      console.log(`Error: ${e}`);
+      res.status(500);
+      res.send(e);
     });
-  });
 
-  pushRequest.on('error', function(e) {
-    console.log(`Error: ${e}`);
-    reply.code(500);
-    reply.send(e);
-  });
+    if (requestData.body) {
+      pushRequest.write(Buffer.from(requestData.body, 'base64'));
+    }
 
-  if (requestData.body) {
-    pushRequest.write(Buffer.from(requestData.body, 'base64'));
+    pushRequest.end();
+  } catch (err) {
+    console.error('Failed to process request', err);
+    res.status(500);
+    res.send('Failed to process request');
   }
-
-  pushRequest.end();
 });
-
-// Run the server and report out to the logs
-(async function() {
-  const address = await fastify.listen({port: process.env.PORT});
-  console.log(`Your app is listening on ${address}`);
-})();
