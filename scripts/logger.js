928ae9acfbd7d45b5c8cd5bf77ac03e4b3afde18
Matt Gaunt <matt@gaunt.dev>
2022-07-28 15:14:54 -0700

    Updating github pages to point to new demo

--- scripts/logger.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env browser */
-
-let l = console;
-if (window['gauntface'] && window['gauntface'].logger) {
-  window['gauntface'].logger.setPrefix('simple-push-demo');
-  l = window['gauntface'].logger;
-}
-
-export const logger = l;
