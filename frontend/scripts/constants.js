d0aaa71708d3711718a6e3aec7e4818472a9102f
Matt Gaunt <matt@gaunt.dev>
2022-07-28 11:13:32 -0700

    Update code comment

--- frontend/scripts/constants.js
+++ frontend/scripts/constants.js
@@ -7,8 +7,9 @@ export const APPLICATION_KEYS = {
   privateKey: 'xKZKYRNdFFn8iQIF2MH54KTfUHwH105zBdzMR7SI3xI',
 };
 
-// This is empty for same origin requests. If the API is ever moved, this shoud
-// be updated.
+// Hosting on vercel will have the API and frontend served from the
+// same origin, so '' is fine.
+// For local development the backend url param can be used.
 const urlParams = new URLSearchParams(window.location.search);
 const backendParam = urlParams.get('backend');
 export const BACKEND_ORIGIN = backendParam ? backendParam : '';
